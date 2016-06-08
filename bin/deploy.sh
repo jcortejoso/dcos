fe22b5c2450c3a5209cb67c92d78cf72cae64937
Carlos García Matín <carlosgarcia@stratio.com>
2016-06-08 11:17:47 +0200

    Fix s3upload call (#19)
    [PAAS-242] Fix s3upload call

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@ echo "Uploading artifact to Nexus repository"
 curl -u stratio:${NEXUSPASS} --upload-file /root/cd/dcos-artifacts/testing/first/dcos_generate_config.sh http://sodio.stratio.com/nexus/content/sites/paas/dcos/${MODULE_VERSION}/dcos_installer-${MODULE_VERSION}.sh
 
 echo "Uploading artifact to s3 repository"
-./s3upload.sh
+bin/s3upload.sh
