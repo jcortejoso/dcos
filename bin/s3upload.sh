fd48b46b20fad5d8a40c7b314a5b7b787a846181
Carlos García Matín <carlosgarcia@stratio.com>
2016-06-13 12:35:28 +0200

    Add permissions for public download from s3 (#24)

--- bin/s3upload.sh
+++ bin/s3upload.sh
@@ -7,12 +7,14 @@ bucket=dcos-packages
 resource="/${bucket}/${filename}"
 contentType="application/x-sh"
 dateValue=`date -R`
-stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+access="x-amz-acl:public-read"
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${access}\n${resource}"
 s3Key=${AWS_ACCESS}
 s3Secret=${AWS_KEY}
 signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
 curl -X PUT -T "${file}" \
   -H "Date: ${dateValue}" \
   -H "Content-Type: ${contentType}" \
+  -H "${access}" \
   -H "Authorization: AWS ${s3Key}:${signature}" \
   https://s3-eu-west-1.amazonaws.com${resource}
