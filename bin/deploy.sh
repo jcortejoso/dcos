82e0cb3adea1b30d44a01e7cd61fa44bcfdaff7b
Antonio Alfonso Pujante <aalfonso@stratio.com>
2016-06-28 13:02:11 +0200

    package and deploy fixes for branching and releasing (#35)
    * package and deploy fixes for branching and releasing
    
    * fix version in s3Upload

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
+BASEDIR=`dirname $0`/..
+VERSION=`cat $BASEDIR/VERSION`
+
 echo "Uploading artifact to Nexus repository"
-curl -u stratio:${NEXUSPASS} --upload-file /root/cd/dcos-artifacts/testing/first/dcos_generate_config.sh http://sodio.stratio.com/nexus/content/sites/paas/dcos/${MODULE_VERSION}/stratio-dcos-${MODULE_VERSION}.sh
+curl -u stratio:${NEXUSPASS} --upload-file /root/cd/dcos-artifacts/testing/first/dcos_generate_config.sh http://sodio.stratio.com/nexus/content/sites/paas/dcos/${VERSION}/stratio-dcos-${VERSION}.sh
 
 echo "Uploading artifact to s3 repository"
 bin/s3upload.sh
