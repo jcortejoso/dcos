654625ab061bab11203242341d817abd4e7f58bb
Ivan Moreno <imoreno@stratio.com>
2016-08-10 13:30:46 +0200

    sfreg: service fabric to consul registry [PAAS-41] (#55)

---
+++ packages/sfreg/extra/sfreg.py
@@ -0,0 +1,110 @@
+#sfreg.py
+import urllib.request
+import requests
+import json
+import logging
+import subprocess
+import pickle
+import os.path
+
+#LOGGER
+logging.basicConfig(level=logging.WARN, format="%(asctime)s - %(levelname)s - %(message)s")
+
+logger = logging.getLogger("sfreg")
+logger.setLevel(logging.INFO)
+
+
+#CONSTANTS
+URL_MESOS = "leader.mesos"
+URL_MASTER = "http://" + URL_MESOS
+URL_CONSUL = URL_MASTER + ":8500"
+URL_3DT = URL_MASTER + ":1050"
+
+
+#FUNCTIONS
+
+def deregisterServiceInConsul( name ):
+    url_deregister = URL_CONSUL + "/v1/agent/service/deregister/" + name
+    return requests.get(url_deregister)
+
+def registerServiceInConsul( name, address, checks ):
+    #{
+    #  "ID": "redis1",
+    #  "Name": "redis",
+    #  "Tags": [
+    #    "master",
+    #    "v1"
+    #  ],
+    #  "Address": "127.0.0.1",
+    #  "Port": 8000,
+    #  "Check": {
+    #    "Script": "/usr/local/bin/check_redis.py",
+    #    "HTTP": "http://localhost:5000/health",
+    #    "Interval": "10s",
+    #    "TTL": "15s"
+    #  }
+    #}
+    url_register = URL_CONSUL + "/v1/agent/service/register"
+    service = {}
+    service["ID"] = name
+    service["Name"] = name
+    service["Address"] = address
+    service["checks"] = checks
+    requests.put(url_register, data=service)
+    return requests.put(url_register, data=json.dumps(service))
+
+def getServiceFabricComponents(): 
+    logger.debug("getServiceFabricComponents from " + URL_MESOS)
+    
+    url_units = URL_3DT + "/system/health/v1/report"
+    
+    logger.debug("Getting units from " + url_units)
+    #READ UNITS
+    with urllib.request.urlopen(url_units) as response:
+        data = json.loads(response.readall().decode('utf-8'))
+    logger.debug(data)
+    return data
+
+def allowedUnitName( name ):
+    return name.endswith(".service")
+
+def formatUnitName( name ):
+    return name.replace(".service","")
+
+def getIP():
+    p = subprocess.Popen(['/opt/mesosphere/bin/detect_ip'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    IP, err = p.communicate()
+    rc = p.returncode
+    return IP.decode("utf-8").rstrip()
+
+#MAIN    
+currentSF = []
+IP = getIP()
+serviceFabricComponents = getServiceFabricComponents()
+
+# Register current service fabric components
+for key, value in serviceFabricComponents["Units"].items():
+    for node in value["Nodes"]:
+        # filter by IP: own services 
+        if node["IP"] == IP and allowedUnitName(value["UnitName"]):
+          name = formatUnitName(value["UnitName"])
+          checks = [{'script':'systemctl status ' + name,'interval':'30s'}]
+          r = registerServiceInConsul(name, IP, checks)
+          logger.info("Registering " + name + " in " + IP + " -> " + str(r.status_code))
+          currentSF.append(name)
+          #r = deregisterServiceInConsul(formatUnitName(value["UnitName"]))
+          #logger.info("Deregistering " + formatUnitName(value["UnitName"]) + " -> " + str(r.status_code))
+
+# Unregister old ones
+# read prev status file
+prevSF = []
+if os.path.isfile("sfstatus"):
+  prevSF = pickle.load(open( "sfstatus", "rb" ))
+
+# Deregister services present in previous synchronization and not now
+for s in set(prevSF) - set(currentSF):
+  r = deregisterServiceInConsul(s)
+  logger.info("Deregistering " + s + " -> " + str(r.status_code))
+
+# save status file for next execution
+pickle.dump(currentSF,open( "sfstatus", "wb" ))
