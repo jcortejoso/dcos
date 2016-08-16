d78c4043eb410940f488a19a5c19802a06abc087
Aitor Mendivil <amendivil@stratio.com>
2016-08-16 10:19:02 +0200

    Add root_ca_cert_path variable. Filebeat modified to work with ssl (#59)

--- packages/filebeat/extra/filebeat_kafka_collector_hosts.sh
+++ packages/filebeat/extra/filebeat_kafka_collector_hosts.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 KAFKA_COLLECTOR_HOSTS=`cat /opt/mesosphere/etc/kafka_collector_hosts`
+ROOT_CA_CERT_PATH=`cat /opt/mesosphere/etc/root_ca_cert_path`
+FILEBEAT_TEMPLATE="filebeat.yml.nossl.template"
 
 if [ -z $KAFKA_COLLECTOR_HOSTS ]
   then
   KAFKA_COLLECTOR_HOSTS="leader.mesos:9092"
 fi
 
+if [ -n "$ROOT_CA_CERT_PATH" ]
+    then
+    FILEBEAT_TEMPLATE="filebeat.yml.ssl.template"
+fi
+
 export KAFKA_COLLECTOR_HOSTS
 
-envsubst '$KAFKA_COLLECTOR_HOSTS' < ${PKG_PATH}/etc/filebeat.yml.template > ${PKG_PATH}/etc/filebeat.yml
+envsubst '$KAFKA_COLLECTOR_HOSTS' < ${PKG_PATH}/etc/$FILEBEAT_TEMPLATE > ${PKG_PATH}/etc/filebeat.yml
