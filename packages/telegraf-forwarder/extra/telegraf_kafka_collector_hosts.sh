3d2e9c80dfebe9fab2b089222b70e9d0611c2d50
Aitor Mendivil <amendivil@stratio.com>
2016-08-22 09:45:14 +0200

    add rootCA path to ssl template in telegraf-forwarder (#64)

--- packages/telegraf-forwarder/extra/telegraf_kafka_collector_hosts.sh
+++ packages/telegraf-forwarder/extra/telegraf_kafka_collector_hosts.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 KAFKA_COLLECTOR_HOSTS=`cat /opt/mesosphere/etc/kafka_collector_hosts`
+ROOT_CA_CERT_PATH=`cat /opt/mesosphere/etc/root_ca_cert_path`
+TELEGRAF_FORWARDER_TEMPLATE="telegraf-forwarder.conf.template"
 
 if [ -z $KAFKA_COLLECTOR_HOSTS ]
   then
   KAFKA_COLLECTOR_HOSTS="master.mesos:9092"
 fi
 
-export KAFKA_COLLECTOR_HOSTS
+if [ -n "$ROOT_CA_CERT_PATH" ]
+    then
+    TELEGRAF_FORWARDER_TEMPLATE="telegraf-forwarder.conf.ssl.template"
+fi
 
-envsubst '$KAFKA_COLLECTOR_HOSTS' < ${PKG_PATH}/etc/telegraf-forwarder.conf.template > ${PKG_PATH}/etc/telegraf-forwarder.conf
+export KAFKA_COLLECTOR_HOSTS
+export ROOT_CA_CERT_PATH
+
+TELEGRAF_FORWARDER_TEMPLATE_AUX="telegraf-forwarder.conf.aux.template"
+
+envsubst '$KAFKA_COLLECTOR_HOSTS' < ${PKG_PATH}/etc/$TELEGRAF_FORWARDER_TEMPLATE > \
+${PKG_PATH}/etc/$TELEGRAF_FORWARDER_TEMPLATE_AUX
+
+envsubst '$ROOT_CA_CERT_PATH' < ${PKG_PATH}/etc/$TELEGRAF_FORWARDER_TEMPLATE_AUX > \
+${PKG_PATH}/etc/telegraf-forwarder.conf
+
+rm -f ${PKG_PATH}/etc/$TELEGRAF_FORWARDER_TEMPLATE_AUX
