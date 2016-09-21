f7679840e90ef102a12fffe7fb726191b04f683d
César Manrique Sánchez <manrique.cms@gmail.com>
2016-09-21 11:15:23 +0200

    Cherry-pick from branch-0.2 (BETA) to master (RC) (#83)
    * Removed docker repo add in prereqs phase (#80)
    
    * PAAS-1210 Update consul to 0.7 (#81)
    
    * PAAS-1210 Update consul to 0.7 - build-info.json cache (#82)

--- ext/dcos-installer/dcos_installer/action_lib/__init__.py
+++ ext/dcos-installer/dcos_installer/action_lib/__init__.py
@@ -328,15 +328,6 @@ fi
 sudo setenforce 0 && \
 sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-
 sudo yum -y update
 
 sudo mkdir -p /etc/systemd/system/docker.service.d
